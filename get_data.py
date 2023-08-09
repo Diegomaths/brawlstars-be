7d5b02c21f1db7d2ebb4dc27c14f1aaf4da20b8e
Diegomaths <Diego.Bellazzi@gruppomol.it>
2023-08-10 00:29:51 +0200

    fix get data

--- get_data.py
+++ get_data.py
@@ -48,12 +48,12 @@ def get_api_data(endpoint):
 #api request
 import datetime
 import os
-new_week_download = datetime.datetime.fromtimestamp(os.path.getmtime("src/data/brawler_list.json")) < datetime.datetime.now().replace(microsecond=0) +datetime.timedelta(days=-7)
+new_week_download = datetime.datetime.fromtimestamp(os.path.getmtime("data/brawler_list.json")) < datetime.datetime.now().replace(microsecond=0) +datetime.timedelta(days=-7)
 
 brawlers_info = get_api_data("https://api.brawlapi.com/v1/brawlers")
 x = brawlers_info["list"]
 n_brawlers = len(x)
-actual_brawlers = len(pd.read_json("src/data/brawler_list.json"))
+actual_brawlers = len(pd.read_json("data/brawler_list.json"))
 refresh_brawlers = n_brawlers > actual_brawlers
 if refresh_brawlers or new_week_download:
     nomi = []
@@ -75,7 +75,7 @@ if refresh_brawlers or new_week_download:
         'starPowers': star_powers
     })
     brawlers_info = brawlers_info.set_index("name")
-    write_json(brawlers_info, "src/data/brawler_list.json")
+    write_json(brawlers_info, "data/brawler_list.json")
 
     #api request function for single brawler by id
 
@@ -105,11 +105,11 @@ if refresh_brawlers or new_week_download:
     gadgets_df = gadgets_df.set_index("name")
 
     gadgets_df["brawler"] = names_g
-    write_json(gadgets_df, "src/data/gadgets_list.json")
+    write_json(gadgets_df, "data/gadgets_list.json")
 
     starpowers_df = starpowers_df.set_index("name")
     starpowers_df = starpowers_df.set_index("name")
-    write_json(starpowers_df, "src/data/starpowers_list.json")
+    write_json(starpowers_df, "data/starpowers_list.json")
 
 ### maps
 
@@ -140,7 +140,7 @@ maps_info["active"] = ~maps_info["active"]
 maps_info=maps_info[maps_info.active]
 maps_info0 = maps_info.copy()
 maps_info = maps_info.set_index("name")
-write_json(maps_info, "src/data/full_maps_list.json")
+write_json(maps_info, "data/full_maps_list.json")
 
 
 maps_info = maps_info0.sort_values(by=["mod","name"])
@@ -149,4 +149,4 @@ maps_info = maps_info.pivot_table(
     values="name",
     aggfunc=list
 )
-write_json(maps_info, "src/data/mods_maps_list.json")
+write_json(maps_info, "data/mods_maps_list.json")
